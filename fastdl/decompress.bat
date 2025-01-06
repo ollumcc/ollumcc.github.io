ee289a13434d90fae8f8796cd77e37559e6008fe
OllumHD <olekpinneberg@gmail.com>
2025-01-06 14:13:11 +0100

    bye fastdl

--- fastdl/decompress.bat
+++
@@ -1,4 +0,0 @@
-@Echo Off
-for /r %%i in (*.bz2) do (
-    "C:\Program Files\7-Zip\7z.exe" e "%%i" -o"%%~dpi"
-)
--- fastdl/leavebz2.bat
+++
@@ -1,4 +0,0 @@
-@Echo Off
-for /r %%i in (*.vtx *.mdl *.vvd *.phy *.mp3 *.wav *.vtf *.bsp *.ztmp *.vmt *.nav) do (
-    del "%%i"
-)
